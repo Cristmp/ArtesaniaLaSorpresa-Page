2644f88cab068e036b2edf4e0cd954049a4103b9
Cristian <cristianzapata2007@gmail.com>
2026-06-12 19:09:21 -0600

    Oferta Settings

---
+++ Js/Ofertas.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", () => {
+    const banners = document.querySelectorAll(".banner-oferta");
+
+    banners.forEach(banner => {
+        banner.addEventListener("click", () => {
+            const productoId = banner.getAttribute("data-id");
+
+            if (productoId) {
+                localStorage.setItem("productoSeleccionado", productoId);
+                window.location.href = "/Pages/InfoProductMarina.html";
+                }
+        });
+    });
+});
