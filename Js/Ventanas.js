7ea7c04138eef6776a1baa88c4cbfa164ae04e53
Cristian <cristianzapata2007@gmail.com>
2026-06-13 19:01:42 -0600

    Logo set / Catálogo last changes

--- Js/Ventanas.js
+++ Js/Ventanas.js
@@ -11,3 +11,20 @@ const AbrirMenuDesplegable = () => {
 };
 
 AbrirMenuDesplegable();
+
+let lastScrollTop = 0;
+const searchingBar = document.querySelector('.searching');
+
+window.addEventListener('scroll', () => {
+    let currentScroll = window.scrollY || document.documentElement.scrollTop;
+    if (currentScroll < 0) currentScroll = 0;
+
+    if (currentScroll > lastScrollTop && currentScroll > 150) {
+
+        searchingBar.classList.add('searching-hidden');
+    } else {
+        searchingBar.classList.remove('searching-hidden');
+    }
+
+    lastScrollTop = currentScroll;
+});
